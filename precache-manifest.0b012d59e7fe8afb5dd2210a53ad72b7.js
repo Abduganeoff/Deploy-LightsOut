0e5a155f0d54f4035b6e1a9f0842104226da303f
Muhammad Rizo <abduganeoff@gmail.com>
2020-12-30 21:28:26 +0100

    Updates

---
+++ precache-manifest.0b012d59e7fe8afb5dd2210a53ad72b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ded5fc3c4f433cf8c9f",
+    "url": "/Deploy-LightsOut/static/css/main.b1482c0e.chunk.css"
+  },
+  {
+    "revision": "0ded5fc3c4f433cf8c9f",
+    "url": "/Deploy-LightsOut/static/js/main.0ded5fc3.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/Deploy-LightsOut/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "7e17a15803a754e1b699",
+    "url": "/Deploy-LightsOut/static/js/runtime~main.7e17a158.js"
+  },
+  {
+    "revision": "2c605dadc5f13381af64cc97bb1e3b44",
+    "url": "/Deploy-LightsOut/index.html"
+  }
+];
